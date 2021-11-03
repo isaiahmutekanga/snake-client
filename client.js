2accea09a195613c8a03e223b4ba3bd86839d915
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2021-11-03 17:11:07 +0000

    updates

--- client.js
+++ client.js
@@ -8,6 +8,10 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Connection established");
+    conn.write("Name: IM");
+  });
 
   return conn;
 };
--- play.js
+++ play.js
@@ -1,24 +1,4 @@
-const net = require("net");
+//const net = require("net");
 const { conn } = require("./client");
-// connection = require("client.js");
-//const myObject = require("./client");
 
-//establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: "165.227.47.243",
-//     port: 50541,
-//   });
-
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-
-//   //console.log("You have been disconnected");
-
-//   return conn;
-// };
-
-//myObject.conn;
 conn();
-
-//connect();
