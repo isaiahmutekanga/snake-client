6a66048cc4d7306cf3177c1467a0818bf40d21b4
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2021-11-03 19:32:26 +0000

    Final edits

--- input.js
+++ input.js
@@ -30,6 +30,9 @@ const handleUserInput = function (key) {
   if (key === "d") {
     connection.write("Move: right");
   }
+  if (key === "m") {
+    connection.write("Say: Hello People");
+  }
 };
 
 //module.exports = { input: setupInput };
